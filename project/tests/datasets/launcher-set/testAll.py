9ddd3782ecf04fb66a181dc1fadefee9fb108065
kreofil <kreofil@mail.ru>
2021-10-25 19:16:22 +0300

    testAll.py corrected

--- project/tests/datasets/launcher-set/testAll.py
+++ project/tests/datasets/launcher-set/testAll.py
@@ -11,13 +11,22 @@ if __name__ == '__main__':
     print(testedFileList)
 
     testCount = 0
+    passCount = 0
     print('Test Started')
     testResults = [] # Информация о результататах
     for testedFile in testedFileList:
         value = os.system(currentDir + '/startTest.py ' + testedFile)
         testCount += 1
-        testResults.append(str(testCount) + ': ' + testedFile + '  ' + str(value))
+        if value == 0:
+            message = 'PASS'
+            passCount += 1
+        else:
+            message = 'FAIL'
+        testResults.append(str(testCount) + ': ' + testedFile + '  ' + message)
 
-    print(testResults)
-    print(f'The End. {testCount} tests evaluated')
+    # print(testResults)
+    for testResult in testResults:
+        print(testResult)
+
+    print(f'The End. {testCount} tests evaluated, {passCount} tests passed')
 
