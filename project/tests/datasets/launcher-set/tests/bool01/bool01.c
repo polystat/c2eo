cac9403ade0382e628e32f26ea789501033b8fa2
kreofil <kreofil@mail.ru>
2021-10-31 22:29:09 +0300

    testOne.py modified

--- project/tests/datasets/launcher-set/tests/bool01/bool01.c
+++ project/tests/datasets/launcher-set/tests/bool01/bool01.c
@@ -2,9 +2,9 @@
 _Bool var = 1;
 //_Bool v = 0;
 int main() {
-//    printf("%d\n", v);
-//    v;
+    printf("%d\n", v);
+    v;
     printf("%d\n", var);
     var;
-//    return 0;
+    return 0;
 }
--- project/tests/datasets/launcher-set/tests/doubleVarDecl/doubleVarDecl.c
+++ project/tests/datasets/launcher-set/tests/doubleVarDecl/doubleVarDecl.c
@@ -8,12 +8,12 @@ double d03 = 3.14;      // не конвертирует
 static double d04 = 3.14;
 
 int main() {
-    printf("%e\n", d01);
+    printf("%f\n", d01);
     d01;
-    printf("%e\n", d02);
+    printf("%f\n", d02);
     d02;
-    printf("%e\n", d03);
+    printf("%f\n", d03);
     d03;
-    printf("%e\n", d04);
+    printf("%f\n", d04);
     d04;
 }
