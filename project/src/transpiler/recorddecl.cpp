613f8454180fe96108d77ffa19f40918d5da8e95
yarrya <yairyabtsev@ispras.ru>
2021-11-05 03:51:02 +0300

    struct or union

--- project/src/transpiler/recorddecl.cpp
+++ project/src/transpiler/recorddecl.cpp
@@ -2,6 +2,9 @@
 #include "generator.h"
 #include "recorddecl.h"
 
-void getRecordDeclSubObjects(const RecordDecl *RD){
-
+void getRecordDeclSubObjects(const RecordDecl* RD) {
+    if (RD->isUnion())
+        llvm::outs() << "Union \n";
+    if (RD->isStruct())
+        llvm::outs() << "Struct \n";
 }
