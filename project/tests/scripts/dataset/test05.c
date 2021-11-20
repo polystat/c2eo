8378548147316446cb16af6d0c244e515d3c616f
yarrya <yairyabtsev@ispras.ru>
2021-11-20 07:57:59 +0300

    +struct05-test

--- project/tests/scripts/dataset/test05.c
+++ project/tests/scripts/dataset/test05.c
@@ -1,4 +1,19 @@
-int main(){
-    {1,2,3,-4,{5!=6,7}};
+struct st {
+    int a;
+    struct st2 {
+        int x, y;
+    } b;
+} a = {1, {2, 3}},
+        b= {5, {6, 7}};
+
+int main() {
+    //a.b.y += 3;
+    a.b.y = a.b.y + 3;
+
+    //b.b.y -= 5;
+    b.b.y = b.b.y - 5;
+
+    //a.b.y *= b.b.x;
+    a.b.y = a.b.y * b.b.x;
     return 0;
 }
