dd0be3e247626fb74e6816083c53702b16abc4ae
Yaroslav Ryabtsev <yairyabtsev@edu.hse.ru>
2022-02-14 13:28:12 +0300

    release refactoring

--- release/repository/app.py
+++ release/repository/app.py
@@ -1,39 +1,8 @@
-# from http.server import HTTPServer, BaseHTTPRequestHandler
-#
-#
-# def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-#     server_address = ('', 8000)
-#     httpd = server_class(server_address, handler_class)
-#     httpd.serve_forever()
-
-# import http.server
-# import socketserver
-#
-# PORT = 8000
-#
-# Handler = http.server.SimpleHTTPRequestHandler
-#
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("serving at port", PORT)
-#     httpd.serve_forever()
-
-# from flask import Flask
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
 from flask import Flask
 from flask_autoindex import AutoIndex
 
 app = Flask(__name__)
-
-ppath = "debian/"
-
-app = Flask(__name__)
-AutoIndex(app, browse_root=ppath)
+AutoIndex(app, browse_root="auto_index/")
 
 if __name__ == "__main__":
     app.run()
--- release/update-release.py
+++ release/update-release.py
@@ -1,10 +1,13 @@
-import argparse
 import datetime
 import os
+import re
 import subprocess
-
 import sys
+
+import argparse
 from git_config import get_config
+
+libs_set = set()
 
 
 def get_user() -> str:
@@ -13,6 +16,7 @@ def get_user() -> str:
 
 
 def inc(version: str) -> str:
+    version = version.replace('-', '.')
     version = version.split('.')
     version[-1] = str(int(version[-1]) + 1)
     return '.'.join(version)
@@ -20,20 +24,89 @@ def inc(version: str) -> str:
 
 def get_version() -> str:
     for file in os.listdir("."):
-        if file[:5] == 'c2eo-':
+        if file[:5] == 'c2eo-' and file[-3:] != 'deb':
             return file[5:]
-    return "?.?.0"
+    return "?.?.1"
+
+
+def try_shell(param):
+    if subprocess.run(param, shell=True).returncode:
+        exit(0)
+
+
+def make_deb(version):
+    for path in libs_set:
+        try_shell(f'cp {path} usr/lib')
+        try_shell(f'cp {path[:-3]} usr/lib')
+    try_shell(f'cp ../../project/bin/c2eo usr/bin')
+    with open('DEBIAN/control', 'r') as control_file:
+        control = control_file.read()
+        control = control.replace('{}.{}-{}'.format(*get_version().split('.')),
+                                  '{}.{}-{}'.format(*version.split('.')))
+    with open('DEBIAN/control', 'w') as control_file:
+        print(control, file=control_file)
+    with open(f'DEBIAN/changelog', 'w') as changelog:
+        print('c2eo ({}.{}-{}) unstable; urgency=medium'.format(*version.split('.')), file=changelog)
+        print(file=changelog)
+        while True:
+            changes = input('describe the new feature: ')
+            if not changes:
+                break
+            print('  *', changes, file=changelog)
+        print(file=changelog)
+        print(' --', args.user, ' ', args.date, file=changelog)
+    try_shell('md5deep -r usr > DEBIAN/md5sums')
+
+
+def make_bin():
+    try_shell('cmake ..')
+    try_shell('make')
+
+
+def make_repo(version):
+    for directory in ['db', 'dists', 'pool']:
+        try_shell(f'rm -rf {directory}')
+    try_shell('reprepro export')
+    try_shell(f'reprepro -P -V -b . -C main includedeb c2eo-rep ../../c2eo-{version}.deb')
+
+
+def host_it():
+    try_shell('az login')
+    try_shell('az webapp up -n c2eo -g demo --location=eastus2 --sku=FREE')
+
+
+def copy_libs():
+    global libs_set
+    flag = True
+    with open('../../release/todo.sh', 'w') as todo:
+        with open('../../llvm-clang/libs.txt', 'r') as libs:
+            for line in libs.readlines():
+                lib_info = re.split(r'\s', line)
+                lib = [e for e in lib_info if e]
+                if len(lib) > 2:
+                    path = os.path.abspath(lib[2])
+                    if path[:6] == '/home/':
+                        print(f'sudo cp {path} /usr/lib', file=todo)
+                        print(f'sudo cp {path[:-3]} /usr/lib', file=todo)
+                        flag = False
+                    if path[-3:] == '.12':
+                        libs_set.add(path)
+    if not flag:
+        print('run todo.sh as root and re-try')
+        exit()
 
 
 if __name__ == '__main__':
     path_to_script = sys.argv[0]
+    path_to_script = os.path.abspath(path_to_script)
     path_to_script = os.path.dirname(path_to_script)
     os.chdir(path_to_script)
+
     parser = argparse.ArgumentParser(description='Release maker')
     parser.add_argument(
         '--branch',
         type=str,
-        default='heap',
+        default='yar',
         help=''
     )
     parser.add_argument(
@@ -54,49 +127,42 @@ if __name__ == '__main__':
         default=get_user(),
         help=''
     )
+    parser.add_argument(
+        '--key',
+        type=str,
+        default='yes',
+        help=''
+    )
     args = parser.parse_args()
-    subprocess.run(f'git checkout {args.branch}', shell=True)
-    subprocess.run(f'mv c2eo-{get_version()} c2eo-{args.version}', shell=True)
+    try_shell(f'git checkout {args.branch}')
 
-    # making bin
-    os.chdir('../project/build')  # or create
-    subprocess.run('cmake ..', shell=True)
-    subprocess.run('make', shell=True)
+    try_shell(f'mv c2eo-{get_version()} c2eo-{args.version}')
 
-    # making deb
-    os.chdir(path_to_script)
-    # todo: subprocess.run(cp -rf /usr/lib/c2eo-lib ./c2eo-{args.version}/usr/lib/c2eo-lib)
-    subprocess.run(f'cp ../project/bin/c2eo ./c2eo-{args.version}/usr/bin', shell=True)
-    with open(f'./c2eo-{args.version}/DEBIAN/control', 'r') as control_file:
-        control = control_file.read()
-        control = control.replace('{}.{}-{}'.format(*get_version().split('.')),
-                                  '{}.{}-{}'.format(*args.version.split('.')))
-    with open(f'./c2eo-{args.version}/DEBIAN/control', 'w') as control_file:
-        print(control, file=control_file)
-    os.chdir(f'./c2eo-{args.version}')
-    with open(f'./DEBIAN/changelog', 'w') as changelog:
-        print('c2eo ({}.{}-{}) unstable; urgency=medium'.format(*args.version.split('.')), file=changelog)
-        print(file=changelog)
-        while True:
-            changes = input('describe the new feature: ')
-            if not changes:
-                break
-            print('  *', changes, file=changelog)
-        print(file=changelog)
-        print(' --', args.user, ' ', args.date, file=changelog)
-    subprocess.run('md5deep -r usr > DEBIAN/md5sums', shell=True)
+    os.chdir('../project')
+    if not os.path.exists('build'):
+        os.mkdir('build')
+    os.chdir('build')
+    make_bin()
+
+    try_shell('ldd ../bin/c2eo > ../../llvm-clang/libs.txt')
+    copy_libs()
+
+    os.chdir(f'../../release/c2eo-{args.version}/')
+    if not os.path.exists('usr'):
+        os.mkdir('usr')
+        os.mkdir('usr/bin')
+        os.mkdir('usr/lib')
+    # make_deb(args.version)
     os.chdir('..')
-    subprocess.run(f'fakeroot dpkg-deb --build c2eo-{args.version}', shell=True)
-    # subprocess.run(f'lintian c2eo-{args.version}.deb', shell=True) ---> checker
+    try_shell(f'fakeroot dpkg-deb --build c2eo-{args.version}')
+    # try_shell(f'lintian c2eo-{version}.deb') ---> checker
 
-    # making repo
     os.chdir('repository/debian')
-    for directory in ['db', 'dists', 'pool']:
-        subprocess.run(f'rm -rf {directory}', shell=True)
-    subprocess.run('reprepro export', shell=True)
-    subprocess.run(f'reprepro -P -V -b . -C main includedeb c2eo-rep ../../c2eo-{args.version}.deb', shell=True)
+    make_repo(args.version)
 
-    # hosting
     os.chdir('..')
-    subprocess.run('az login', shell=True)
-    subprocess.run('az webapp up -n c2eo -g demo --location=eastus2 --sku=FREE', shell=True)
+    if not os.path.exists('auto_index'):
+        os.mkdir('auto_index')
+    try_shell(f'cp -rf debian/pool auto_index')
+    try_shell(f'cp -rf debian/dists auto_index')
+    host_it()
