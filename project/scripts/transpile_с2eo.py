88f69d5423269742a63deb4d6824d663148f07b1
Максим <kyngfy-panda@yandex.ru>
2022-03-14 20:12:40 +0300

    update test system

--- project/scripts/transpile_с2eo.py
+++ project/scripts/transpile_с2eo.py
@@ -10,7 +10,6 @@ import re as regex
 # Our scripts
 import tools
 import settings
-import collect_transpiled_code
 import clean_before_transpilation
 
 
@@ -21,7 +20,6 @@ class Transpiler(object):
         if os.path.isfile(path_to_c_files):
             path_to_c_files = os.path.dirname(path_to_c_files)
         self.path_to_c_files = os.path.join(path_to_c_files, '')
-        self.path_to_assembly = settings.get_setting('path_to_assembly')
         self.path_to_eo_src = settings.get_setting('path_to_eo_src')
         self.path_to_eo_project = settings.get_setting('path_to_eo_project')
         self.path_to_c2eo = settings.get_setting('path_to_c2eo')
@@ -41,7 +39,7 @@ class Transpiler(object):
     def start_transpilation(self, path_to_c_file):
         eo_c_file = self.prepare_eo_c_file(path_to_c_file)
         output_name = tools.get_file_name(eo_c_file).replace('-eo', '')
-        transpile_cmd = f'{self.path_to_c2eo}c2eo {eo_c_file} {output_name} > /dev/null'
+        transpile_cmd = f'{self.path_to_c2eo}c2eo {eo_c_file} {output_name}.eo > /dev/null'
         subprocess.run(transpile_cmd, shell=True)
         return os.path.abspath(eo_c_file)
 
@@ -60,9 +58,6 @@ class Transpiler(object):
         return prepared_eo_c_file
 
     def start_collecting(self, eo_c_files):
-        for file in eo_c_files:
-            name = tools.get_file_name(file).replace('-eo', '')
-            collect_transpiled_code.main(name)
         self.remove_unused_eo_files()
         difference = tools.thread_pool().map(self.move_transpilation_files, eo_c_files)
         difference = list(filter(lambda x: x, difference))  # Filter None values
@@ -74,7 +69,7 @@ class Transpiler(object):
             tools.pprint('\nNot found any changes in src files')
 
     def remove_unused_eo_files(self):
-        eo_assembly_names = tools.search_files_by_pattern(self.path_to_assembly, '*.eo')
+        eo_assembly_names = tools.search_files_by_pattern('.', '*.eo')
         eo_assembly_names = set(map(tools.get_file_name, eo_assembly_names))
         src_eo_names = tools.search_files_by_pattern(self.path_to_eo_src, '*.eo')
         src_eo_names = set(map(tools.get_file_name, src_eo_names))
@@ -83,19 +78,19 @@ class Transpiler(object):
 
     def move_transpilation_files(self, eo_c_file):
         name = tools.get_file_name(eo_c_file).replace('-eo', '')
-        assembly_file = f'{self.path_to_assembly}{name}.eo'
+        transpilation_file = f'{name}.eo'
         eo_c_dir = os.path.dirname(eo_c_file)
         for extension in ['.glob', '.stat']:
-            file = assembly_file.replace('.eo', extension)
+            file = transpilation_file.replace('.eo', extension)
             if os.path.isfile(file):
                 shutil.move(file, eo_c_dir)
-        shutil.copy(assembly_file, eo_c_dir)
+        shutil.copy(transpilation_file, eo_c_dir)
         src_file = f'{self.path_to_eo_src}{name}.eo'
-        if not tools.compare_files(assembly_file, src_file):
-            shutil.move(assembly_file, src_file)
-            return assembly_file
-        if os.path.isfile(assembly_file):
-            os.remove(assembly_file)
+        if not tools.compare_files(transpilation_file, src_file):
+            shutil.move(transpilation_file, src_file)
+            return transpilation_file
+        if os.path.isfile(transpilation_file):
+            os.remove(transpilation_file)
 
     def generate_run_sh(self, eo_c_file):
         file_name = tools.get_file_name(eo_c_file).replace('-eo', '')
