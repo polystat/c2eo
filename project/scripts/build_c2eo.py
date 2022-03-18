c7ffd3af11039c2d633c68a3d201b565d12735d6
Максим <kyngfy-panda@yandex.ru>
2022-03-18 22:28:02 +0300

    build dir for transpiled files + remove empty tests

--- project/scripts/build_c2eo.py
+++ project/scripts/build_c2eo.py
@@ -9,12 +9,12 @@ import tools
 import settings
 
 
-def main(path_to_c2eo=None):
-    if path_to_c2eo is None:
-        path_to_c2eo = settings.get_setting('path_to_c2eo')
+def main(path_to_c2eo_transpiler=None):
+    if path_to_c2eo_transpiler is None:
+        path_to_c2eo_transpiler = settings.get_setting('path_to_c2eo_transpiler')
     tools.pprint()
     original_path = os.getcwd()
-    os.chdir(path_to_c2eo)
+    os.chdir(path_to_c2eo_transpiler)
     subprocess.run('cmake ..', shell=True)
     subprocess.run('make', shell=True)
     os.chdir(original_path)
