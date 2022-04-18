039c44a0fdaf0ce8408078650c8b3180178580e4
yarrya <yairyabtsev@edu.hse.ru>
2022-04-19 00:31:47 +0300

    list init

--- project/tests/in_progress/struct/struct00.c
+++ project/tests/in_progress/struct/struct00.c
@@ -1,19 +1,17 @@
-typedef struct st1 st1;
-typedef struct st2 st2;
+//typedef struct st st;
+//
+//struct st{
+//  short a;
+//  int b;
+//  long c;
+//  long long d;
+//};
+//
+//st obj1 = {1, 2, 3, 4};
 
-struct st2{
-  int a, b, c[3];
-};
-
-struct st1{
-  st2 a, b[3], c;
-} obj[5];
+double a[3] = {1.,2.9,3.8};
 
 int main() {
-  obj[2].b[2].c[2] = 7;
-  obj[2].b[2].c[2];
-//  2[obj[2].b[2].c];
-//  2[2[obj[2].b].c];
-//  2[2[2[obj].b].c];
+//  st obj2 = {2, 3, 4, 5};
   return 0;
 }
