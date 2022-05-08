0e1a268f218d9002857c2f041a4142dbcfa67055
LeDron12 <HolyMiracle580@gmail.com>
2022-05-08 20:55:29 +0300

    multidim static arrays added

---
+++ project/tests/main/staticArray/staticArray13.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+//clang -Xclang -ast-dump -fsyntax-only func01.c
+
+long long a[5][7];
+int main() {
+    a[1][3] = 10;
+    a[3][1] = 20;
+      printf("%lld\n", a[1][3]);
+    a[1][3];
+      printf("%lld\n", a[3][1]);
+    a[3][1];
+    return 0;
+}
