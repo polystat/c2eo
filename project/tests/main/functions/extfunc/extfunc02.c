68d3c12e88d8b97d5de2c7721d1d092e0be1a026
al <kreofil@mail.ru>
2022-05-28 20:05:19 +0300

    Tests of external functions added

---
+++ project/tests/main/functions/extfunc/extfunc02.c
@@ -0,0 +1,18 @@
+//clang -Xclang -ast-dump -fsyntax-only main.c
+#include <stdio.h>
+long long c;
+
+extern long long f();
+
+int main() {
+    c = f();
+    printf("%lld", c);
+    return 0;
+}
+
+extern long long f();
+
+long long f() {return 100;}
+
+extern long long f();
+
