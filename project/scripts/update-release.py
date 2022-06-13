2ae6df328db32ad314d2265ca1d8ee9d35b7f1ee
yarrya <yairyabtsev@edu.hse.ru>
2022-06-13 13:58:37 +0300

    release update

--- project/scripts/update-release.py
+++ project/scripts/update-release.py
@@ -194,9 +194,10 @@ if __name__ == '__main__':
     control['c2eo']['Maintainer'] = user
     control['llvm-libs']['Maintainer'] = user
 
-    os.chdir('../project')
+    os.chdir('../')
     os.makedirs('build', exist_ok=True)
     os.chdir('build')
+    os.makedirs('../../release/', exist_ok=True)
     make_bin()
 
     try_shell('ldd ../bin/c2eo > ../../llvm-clang/libs.txt')
@@ -223,4 +224,4 @@ if __name__ == '__main__':
     os.chdir('..')
     sign()
 
-    s3put()
+    # s3put()
