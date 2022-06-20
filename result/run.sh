37e4fd98c4b6c78d4fb3b1a51bdc83380589f8a8
nkchuykin <nkchuykin@edu.hse.ru>
2022-06-20 19:59:51 +0300

    Fix write generation to write-as-... in function call and return stmt

--- result/run.sh
+++ result/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp target/classes:target/eo-runtime.jar org.eolang.Main c2eo.src.do_while.do_local.global "$@"
+java -cp target/classes:target/eo-runtime.jar org.eolang.Main c2eo.src.switch.switch.global "$@"
