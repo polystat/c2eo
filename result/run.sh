9b8aed7e594cd331c5d5974ee24842b134bb0c6e
kreofil <kreofil@mail.ru>
2022-06-28 06:02:29 +0300

    The run.sh only

--- result/run.sh
+++ result/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp target/classes:target/eo-runtime.jar org.eolang.Main c2eo.src.good_101_220.00101.00101.global "$@"
+java -cp target/classes:target/eo-runtime.jar org.eolang.Main c2eo.src.00007.00007.global "$@"
