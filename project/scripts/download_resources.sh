0e08d5c791e2ac3f95e1ab8bef7f0d146057d486
Eugene Dar <evgenedar@mail.ru>
2023-01-09 21:48:55 +0300

    downloading puzzle created

--- project/scripts/download_resources.sh
+++ project/scripts/download_resources.sh
@@ -1,3 +1,9 @@
 #wget "https://github.com/polystat/c2eo/tree/master/project/eo-lib"
 echo copy eo-lib to "$1"
 cp -r ./../eo-lib/ "$1"
+
+# @todo #1049:30min In this implementation, we simply
+#  copy the current implementation, which lies at the
+#  relative address locally. We have to download the
+#  current version from the repository. The address
+#  is given above in the comment.
